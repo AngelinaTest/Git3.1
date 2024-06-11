074cdfcdbf5476960e92b29a411bc26c532bd32e
AngelinaTest <tatiana.i.97@mail.ru>
2024-06-11 10:21:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balance = 100;
-        int add = 500;
-        int bonus = 0;
-
-        if (add > 1000) {
-            bonus = add / 100;
-        }
-        int finalBalance = balance + add + bonus;
-        System.out.println("Итоговый счет:" + finalBalance);
-        System.out.println("Бонус: " + bonus);
+        int price = 13_676;
+        int miles = price / 20;
+        System.out.println("Бонус: " + miles);
     }
 }
