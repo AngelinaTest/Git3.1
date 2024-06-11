eb8323334f8ccc96100fc5ce66422c1f9d158609
AngelinaTest <tatiana.i.97@mail.ru>
2024-06-11 10:16:48 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int add = 500;
+        int bonus = 0;
+
+        if (add > 1000) {
+            bonus = add / 100;
+        }
+        int finalBalance = balance + add + bonus;
+        System.out.println("Итоговый счет:" + finalBalance);
+        System.out.println("Бонус: " + bonus);
+    }
+}
